efebe2f91f5497dc24f5a6e85f207d03109f73a4
afarionik <vaeritas6055@gmail.com>
2017-08-18 21:05:18 +0300

    test dogs

--- cat_test.py
+++ cat_test.py
@@ -9,6 +9,12 @@ class TestPets(unittest.TestCase):
         cat=Cat(name='Kitty', hates_dogs=True)
 
         self.assertEqual("Kitty", cat.name)
+        
+    def test_dogs(self):
+        
+        dog=Dog(name='Bars', chases_cats=True)
+
+        self.assertEqual(True, dog.chases_cats)
 
 if __name__ == '__main__':
     unittest.main()
