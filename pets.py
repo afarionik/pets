fb5af1b48aebda6e9a5bbd55db2d7a5f5a8f2896
afarionik <vaeritas6055@gmail.com>
2017-08-18 20:16:51 +0300

    Update pets.py

--- pets.py
+++ pets.py
@@ -11,3 +11,11 @@ class Pet(object):
 
     def __str__(self):
         return "{} is a {}". format(self.name, self.get_species)
+    
+class Dog(Pet):
+    def __init__ (self, name, chases_cats):
+        Pet.__init__(self, name, 'Dog')
+        self.chases_cats = chases_cats
+
+    def chases_cats(self):
+        return self.chases_cats
